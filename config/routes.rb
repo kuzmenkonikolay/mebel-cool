ff6dbb6b46b1c70123bdca381b4d2ce43545c56a
Nikolay Kuzmenko <kuzmenkojob@gmail.com>
2017-11-10 16:49:21 +0300

    add devise and active admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   root 'mains#index'
 end
---
+++ db/migrate/20171110134853_create_furniture.rb
@@ -0,0 +1,6 @@
+class CreateFurniture < ActiveRecord::Migration[5.1]
+  def change
+    create_table :furnitures do |t|
+    end
+  end
+end
